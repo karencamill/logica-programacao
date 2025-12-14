1b42ce9d97955780a77cc0023663dc168068aaba
Karen Camilly <129320903+karencamill@users.noreply.github.com>
2025-12-14 00:04:02 -0300

    contagem numeros impares

--- exe_contagem/src/main/java/Impares.java
+++ exe_contagem/src/main/java/Impares.java
@@ -7,7 +7,7 @@ public class Impares {
 
         int contadorImpares = 0;
 
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= 10; i++) {
             System.out.print("Digite o " + i + "º número: ");
             int num = sc.nextInt();
 
@@ -23,3 +23,15 @@ public class Impares {
 
     }
 }
+
+
+/* 9 - Contagem de números ímpares > Peça ao usuário 10 números. Conte quantos deles são ímpares e mostre o total ao final.
+
+Contagem de números ímpares — Resposta (escrita)
+Criar uma variável para contar a quantidade de números ímpares, iniciando em 0.
+Pedir ao usuário que informe 10 números.
+Para cada número informado:
+Verificar se o número é ímpar.
+Se for ímpar, aumentar o contador em 1.
+Após receber os 10 números, exibir a quantidade total de números ímpares informados.
+*/
