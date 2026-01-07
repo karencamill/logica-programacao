6f20cfd18cc8a13540e3e467cad6eba6e09a06b6
Karen Camilly <129320903+karencamill@users.noreply.github.com>
2026-01-07 02:05:57 -0300

    media apenas dos pares

---
+++ exe_08/src/main/java/Program.java
@@ -0,0 +1,49 @@
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.logging.SocketHandler;
+
+public class Program {
+    public static void main(String[] args) {
+        /*2. Média apenas dos pares
+Peça ao usuário vários números inteiros até que ele digite 0.
+Calcule e mostre a média apenas dos números pares digitados.*/
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int soma = 0;
+        int quantidade = 0;
+        double media = 0;
+
+        System.out.print("Digite um número: ");
+        int num = sc.nextInt();
+
+        while (num != 0) {
+
+            if (num % 2 == 0) {
+                soma += num;
+                quantidade ++;
+            }
+
+            System.out.print("Digite um número: ");
+            num = sc.nextInt();
+
+        }
+
+        if (quantidade > 0) {
+            media = (double) soma / quantidade;
+        }
+        else {
+            System.out.println();
+            System.out.println("Nenhum número par digitado.");
+        }
+
+        System.out.println();
+        System.out.println("A soma dos números pares: " + soma);
+        System.out.println("Quantidade de números pares: " + quantidade);
+        System.out.printf("A média dos números pares é: %.2f" , media);
+
+        sc.close();
+
+    }
+}
