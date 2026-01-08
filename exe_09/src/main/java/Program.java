54c47d5d5b96b48e6cd292f61cbeef4eab6f4dc2
Karen Camilly <129320903+karencamill@users.noreply.github.com>
2026-01-08 02:35:25 -0300

    Add files via upload

---
+++ exe_09/src/main/java/Program.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        /*3. Sequência crescente
+
+Peça ao usuário vários números inteiros.
+O programa deve continuar lendo valores até que o usuário digite 0.
+Verifique se os números foram digitados em ordem crescente.*/
+
+        Scanner sc = new Scanner(System.in);
+
+        int anterior;
+        int atual;
+        boolean crescente = true;
+
+        System.out.print("Digite um número: ");
+        anterior = sc.nextInt();
+
+        if (anterior == 0) {
+            System.out.println();
+            System.out.println("Nenhuma sequência foi informada.");
+            sc.close();
+            return;
+        }
+
+        while (true) {
+            System.out.print("Digite um número: ");
+            atual = sc.nextInt();
+
+            if (atual == 0) {
+                break;
+            }
+
+            if (atual <= anterior) {
+                crescente = false;
+            }
+
+            anterior = atual;
+        }
+
+        if (crescente) {
+            System.out.println();
+            System.out.println("A sequencia é crescente");
+        }
+        else {
+            System.out.println();
+            System.out.println("A sequencia não é crescente");
+        }
+
+        sc.close();
+
+    }
+}
